38afb11f602f4dc7fe26f1465c0889daf8a40e31
zainab <zainab_ali_juma@hotmail.com>
2026-09-16 12:36:19 +0300

    admin-panel-p7

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -6,26 +6,33 @@ const User = require("../models/User.js");
 
 
 
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
     res.render('homepage.ejs')
 })
 
-router.get('/admin', isAdmin, async (req, res)=>{
-  const allUsers = await User.find()
-  res.render('all-users.ejs', {allUsers: allUsers})
+router.get('/admin', isAdmin, async (req, res) => {
+    const allUsers = await User.find()
+    res.render('all-users.ejs', { allUsers: allUsers })
 })
 
 
-router.post('/toggle-admin', isAdmin, async (req, res)=>{
+router.post('/toggle-admin', isAdmin, async (req, res) => {
 
-  const toggledUsers = await User.findById(req.body.userId );
+    const toggledUsers = await User.findById(req.body.userId);
 
- toggledUsers.isAdmin = !toggledUsers.isAdmin
-  await toggledUsers.save();
-    
-  res.redirect('/admin')
+    toggledUsers.isAdmin = !toggledUsers.isAdmin
+    await toggledUsers.save();
+
+    res.redirect('/admin')
 })
 
+
+router.post('/delete-user', isAdmin, async (req, res) => {
+
+    await User.findByIdAndDelete(req.body.userId);
+
+    res.redirect('/admin')
+})
 
 
 module.exports = router;
