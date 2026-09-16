e974f8f72548a1341478c552cf12f84b4ef1d6ff
zainab <zainab_ali_juma@hotmail.com>
2026-09-16 11:27:14 +0300

    admin-panel-p5

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,19 @@
 const router = require("express").Router()
 const isSignedIn = require('../middleware/is-signed-in')
+const isAdmin = require("../middleware/is-admin.js");
+const User = require("../models/User.js");
+
+
 
 
 router.get('/', (req,res)=>{
     res.render('homepage.ejs')
 })
+
+router.get('/admin', isAdmin, async (req, res)=>{
+  const allUsers = await User.find()
+  res.render('all-users.ejs', {allUsers: allUsers})
+})
+
+
 module.exports = router;
